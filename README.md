85b35fc21b967963fa8a5c3c41eb9ef0b8dd52b2
Pedro H. Spagiari <phspagiari@gmail.com>
2018-10-05 16:56:48 -0300

    feat(*) ServiceAccount is mandatory

--- README.md
+++ README.md
@@ -4,8 +4,6 @@ Monitors spot termination signal and gracefully drain the node before terminatio
 AWS returns the termination time on `http://169.254.169.254/latest/meta-data/spot/termination-time` 2 minutes before the instance terminates.
 The idea behind this simple script is to drain gracefully the node before AWS abruptly terminate the instance.
 
-I strongly suggest you to **always** use with a ServiceAccount instead of a token, for sanity's sake.
-
 ## How to use / deploy
 1. Update the daemonset file with your needs
 2. Apply everything: `kubectl apply -f deploy/`
--- run.sh
+++ run.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
 trap "exit 0" SIGINT SIGTERM
 
+
 DEBUG=${DEBUG:-false}
 TERMINATION_ENDPOINT="http://169.254.169.254/latest/meta-data/spot/termination-time"
-SA_TOKEN="$(cat /var/run/secrets/kubernetes.io/serviceaccount/token 2>/dev/null)"
-
-
-[[ -n $SA_TOKEN ]] && [[ ${DEBUG} == true ]] && echo "[$(date '+%Y-%m-%d %H:%M:%S')] DEBUG: Using ServiceAccount Token"
-[[ -n $TOKEN ]] && [[ ${DEBUG} == true ]] && echo "[$(date '+%Y-%m-%d %H:%M:%S')] DEBUG: Using TOKEN EnvVar"
-TOKEN=${TOKEN:-$SA_TOKEN}
-[[ -z $TOKEN ]] && [[ ${DEBUG} == true ]] && echo "[$(date '+%Y-%m-%d %H:%M:%S')] DEBUG: Empty credentials; Service may not work"
 
 
 echo "[$(date '+%Y-%m-%d %H:%M:%S')] INFO: Starting on node ${MY_NODE_NAME}"
 echo "[$(date '+%Y-%m-%d %H:%M:%S')] INFO: polling AWS meta-data every 5 seconds"
+
 while true; do
   status_code=$(curl -s -o /dev/null -w "%{http_code}" "${TERMINATION_ENDPOINT}")
   if [[ ${status_code} == "200" ]]; then
